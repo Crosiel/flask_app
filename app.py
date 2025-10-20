ad78b32860baeaf40868571a97eb0c5f1743233c
Lucas Carpintero Tavares De Mattos <00001124521756sp@aluno.educacao.sp.gov.br>
2025-10-20 12:01:57 -0300

    Primeiro commit - Aplicação Flask simples

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Olá, mundo! 🚀 Flask funcionando! auau"
+    return "Olá, mundo! 🚀 Flask funcionando!"
 
 if __name__ == '__main__':
     app.run(debug=True)
